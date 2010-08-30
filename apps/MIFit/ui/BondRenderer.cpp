35efe359f7208c32e769ea75263fa5bf68998d92
Bradley A. Smith <bradley@baysmith.com>
2010-08-29 21:28:02 -0700

    Only render bond midpoint if color changes

--- apps/MIFit/ui/BondRenderer.cpp
+++ apps/MIFit/ui/BondRenderer.cpp
@@ -6,16 +6,26 @@ BondRenderer::BondRenderer()
 
 void BondRenderer::addBond(const QVector3D &pos1, const QVector4D &color1, const QVector3D &pos2, const QVector4D &color2)
 {
-    QVector3D midPoint = 0.5 * (pos1 + pos2);
+    if (color1 == color2)
+    {
+        vertices.append(pos1);
+        colors.append(color1);
+        vertices.append(pos2);
+        colors.append(color2);
+    }
+    else
+    {
+        QVector3D midPoint = 0.5 * (pos1 + pos2);
 
-    vertices.append(pos1);
-    colors.append(color1);
-    vertices.append(midPoint);
-    colors.append(color1);
-    vertices.append(midPoint);
-    colors.append(color2);
-    vertices.append(pos2);
-    colors.append(color2);
+        vertices.append(pos1);
+        colors.append(color1);
+        vertices.append(midPoint);
+        colors.append(color1);
+        vertices.append(midPoint);
+        colors.append(color2);
+        vertices.append(pos2);
+        colors.append(color2);
+    }
 }
 
 void BondRenderer::draw() const
